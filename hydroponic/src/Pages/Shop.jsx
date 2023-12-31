18cd9b2d7b10bca26d73f08a6d40db84b1925e73
uraharaSky <akashbiswaliit7@gmail.com>
2023-12-31 06:15:42 +0530

    akash's part

--- hydroponic/src/Pages/Shop.jsx
+++ hydroponic/src/Pages/Shop.jsx
@@ -1,5 +1,71 @@
 import React from "react";
+import React from 'react';
+import ProductCard from 'productCard'; 
+import Sidebar from 'Sidebar'; 
+import Footer from 'Footer'; 
+
+const Shop = () => {
+  return (
+    <div className="shop-page">
+      <Sidebar />
+
+     
+      <div className="product-cards">
+       
+        <ProductCard
+          image="product1.jpg"
+          name="Product 1"
+          price="100"
+          description="Description for Product 1"
+        />
+        <ProductCard
+          image="product2.jpg"
+          name="Product 2"
+          price="100"
+          description="Description for Product 2"
+        />
+        
+        <ProductCard
+          image="product3.jpg"
+          name="Product 3"
+          price="100"
+          description="Description for Product 3"
+        />
+       <ProductCard
+          image="product4.jpg"
+          name="Product 4"
+          price="100"
+          description="Description for Product 3"
+        />
+          <ProductCard
+          image="product5.jpg"
+          name="Product 5"
+          price="100"
+          description="Description for Product 3"
+        />
+         <ProductCard
+          image="product6.jpg"
+          name="Product 6"
+          price="100"
+          description="Description for Product 3"
+        />
+         <ProductCard
+          image="product7.jpg"
+          name="Product 7"
+          price="100"
+          description="Description for Product 3"
+        />
+      </div>
+
+      
+      <Footer />
+    </div>
+  );
+};
+
+
+
 
 export default function Shop() {
-  return <div>SHoping</div>;
+  return <div>Shoping</div>;
 }
---
+++ hydroponic/src/Pages/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="brand">Hydroponics</div>
+      <div className="contact">
+        <p>Contact Us:</p>
+        <p>Phone: 123-456-7890</p>
+        <p>Email: info@example.com</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
